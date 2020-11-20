34e10ff2509f23aefd61a20658ad082758d9a5be
Joseph Perry <josephperry720@gmail.com>
2020-11-20 15:39:25 -0500

    folium map on home page

--- world/views.py
+++ world/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Destinations
 import json
+import folium
 
 # Create your views here.
 def home_view(request):
@@ -15,6 +16,8 @@ def home_view(request):
         form = Destinations(country_name=choice['country_name'], been=been, want_to_go=want_to_go)
         form.save()
         return redirect('myworld')
+    m = folium.Map(location=[35, 0], zoom_start=1.5, zoom_control=False, control_scale=False, no_touch=True, min_zoom=2)
+    m.save('world/templates/world/map.html')
     # print(request.POST)
     with open('world/json/world_countries.json', 'r') as file:
         data = json.load(file)
