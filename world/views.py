0bd4293558680079153278417b8cf8d9eefd1bea
Joseph Perry <josephperry720@gmail.com>
2020-11-20 14:47:36 -0500

    post request for countries

--- world/views.py
+++ world/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Destinations
 import json
 
 # Create your views here.
 def home_view(request):
+    if request.POST:
+        choice = request.POST
+        been = False
+        want_to_go = False
+        if choice['status'] == 'Want to go!':
+            want_to_go = True
+        if choice['status'] == 'Already been!':
+            been = True
+        form = Destinations(country_name=choice['country_name'], been=been, want_to_go=want_to_go)
+        form.save()
+        return redirect('myworld')
+    # print(request.POST)
     with open('world/json/world_countries.json', 'r') as file:
         data = json.load(file)
     context = {
