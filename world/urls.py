145c70f582413a934add7cdbb64f2927170980b8
Joseph Perry <josephperry720@gmail.com>
2020-11-20 12:01:25 -0500

    working navbar

--- world/urls.py
+++ world/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^myworld$', views.world_view, name='myworld'),
     url(r'', views.home_view, name='home')
 ]
--- world/views.py
+++ world/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(request):
     return render(request, 'world/home.html', {})
+
+def world_view(request):
+    return render(request, 'world/myworld.html', {})
