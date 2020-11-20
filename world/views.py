c267dfd074fab494809ecc90834ac45fff6110a7
Joseph Perry <josephperry720@gmail.com>
2020-11-20 13:44:34 -0500

    added form with countries

--- world/views.py
+++ world/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+import json
 
 # Create your views here.
 def home_view(request):
-    return render(request, 'world/home.html', {})
+    with open('world/json/world_countries.json', 'r') as file:
+        data = json.load(file)
+    context = {
+        'countries': data['features']
+    }
+    return render(request, 'world/home.html', context)
 
 def world_view(request):
     return render(request, 'world/myworld.html', {})
