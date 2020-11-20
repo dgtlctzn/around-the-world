99dd3d6979f3c230dd1b843edd36f6f252a9e822
Joseph Perry <josephperry720@gmail.com>
2020-11-20 16:39:58 -0500

    added choropleth maps

--- world/views.py
+++ world/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Destinations
 import json
 import folium
+import pandas as pd
 
 # Create your views here.
 def home_view(request):
@@ -27,8 +28,26 @@ def home_view(request):
     return render(request, 'world/home.html', context)
 
 def world_view(request):
+    my_countries = Destinations.objects.all()
+    # print(my_countries)
+    country_dict = {}
+    for n, country in enumerate(my_countries):
+        # print(country.country_name)
+        # if country['country_name'] not in country_dict:
+        country_dict[n] = {'name': country.country_name, 'been': country.been}
+    countries = pd.DataFrame.from_dict(country_dict, orient='index')
+    print(countries)
+    my_map = folium.Map(location=[35, 0], zoom_start=1.5, zoom_control=False, control_scale=False, no_touch=True, min_zoom=2)
+    folium.Choropleth(geo_data='world/json/world_countries.json',
+                 name='My Countries',
+                 data=countries,
+                 columns=['name', 'been'],
+                 key_on='feature.properties.name',
+                 fill_color='BuGn',
+                 nan_fill_color='white'
+                ).add_to(my_map)
+    my_map.save('world/templates/world/my_map.html')
     context = {
-        'destinations': Destinations.objects.all()
+        'destinations': my_countries
     }
-    print(context)
     return render(request, 'world/myworld.html', context)
