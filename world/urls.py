5dc818bd9c9070748aa54ec086b513baef814e9d
Joseph Perry <josephperry720@gmail.com>
2020-11-26 14:05:31 -0500

    added homw page back in

--- world/urls.py
+++ world/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^signin$', views.sign_in_view, name='signin'),
     url(r'^signup$', views.sign_up_view, name='signup'),
     url(r'^myworld/(?P<user_id>[0-9]+)/$', views.world_view, name='myworld'),
-    # url(r'', views.home_view, name='home')
+    url(r'', views.home_view, name='home')
 ]
