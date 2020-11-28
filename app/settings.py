190729a584d78911c622586063f73bba053d8814
Joseph Perry <josephperry720@gmail.com>
2020-11-28 10:50:22 -0500

    changed allowed host

--- app/settings.py
+++ app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'https://go-around-the-world.herokuapp.com/']
 
 
 # Application definition
